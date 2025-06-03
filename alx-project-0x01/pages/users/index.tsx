3a81d4d88292fd8f51a2e44b751a5bb8cc8030fa
Abdelhamid_Marmar <abdelhamidmarmar4@gmail.com>
2025-06-03 23:19:09 +0100

    Update index.tsx

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -4,4 +4,16 @@ const Users = () => {
   return <div className="p-6">This is the Users page</div>;
 };
 
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
+
 export default Users;
